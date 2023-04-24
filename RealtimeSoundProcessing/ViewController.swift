1cc270ed2b5dadf2056b9db7ae84e6861e80afc1
Masaki <anthrgrnwrld@gmail.com>
2023-04-24 22:31:13 +0900

    Initial Commit

---
+++ RealtimeSoundProcessing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealtimeSoundProcessing
+//
+//  Created by Masaki Horimoto on 2023/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
